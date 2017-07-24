4314396c5c67d5b0d43687be931778b0267acd42
XiongQiuQiu <zhangjinwei94@163.com>
2017-07-24 20:56:37 +0800

    test

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, jsonify
 import jieba.posseg
 app = Flask(__name__)
+def cut_ansj(sentence):
+    words = jieba.posseg.cut(sentence=sentence, HMM=True)
+    ans = []
+    for word, flag in words:
+        if flag == 'nr':
+            ans.append('/'.join([word, flag]))
+    return ','.join(ans)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    sentence = u'姚晨和凌潇肃离婚了'
+    ner_cut = cut_ansj(sentence)
+    return jsonify({'result': 'success', 'cut_result': ner_cut})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
