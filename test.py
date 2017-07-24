dfebacadeaa2716a5c9d214c85d4e2b632cd4a69
XiongQiuQiu <zhangjinwei94@163.com>
2017-07-24 20:30:39 +0800

    add test

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-import sys
-sys.path.append("../")
-
-import jieba
+from flask import Flask
 import jieba.posseg
-import jieba.analyse
+app = Flask(__name__)
 
-def cut_sentence(sentence):
-    seg_list = jieba.cut(sentence=sentence, HMM=True)
-    print(", ".join(seg_list))
-    seg_list1 = jieba.cut(sentence=sentence, HMM=False)
-    print(", ".join(seg_list1))
-    words = jieba.posseg.cut(sentence, HMM=True)
-    for word, flag in words:
-        if flag=='nr':
-            print '/'.join([word, flag])
-print 'test'
-#cut_sentence("姚晨和凌潇肃离婚了")
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
